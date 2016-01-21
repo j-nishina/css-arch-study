b9b9c7e8c6b7477f6bac8b32798778a39d3deed9
Junpei NISHINA <junpei.nishina@bizreach.co.jp>
2016-01-22 01:28:01 +0900

    add a project for smacss sample

---
+++ smacss/gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var sass = require('gulp-sass');
+var webserver = require('gulp-webserver');
+var plumber = require('gulp-plumber');
+
+// 開発用webサーバ起動タスク
+gulp.task('serve', function () {
+  gulp.src('dest')
+    .pipe(webserver({
+      host: 'localhost',
+      port: 3000,
+      livereload: true
+    }));
+});
+
+// scssファイルのコンパイル用タスク
+gulp.task('sass', () => {
+  gulp.src(['./src/scss/**/*.scss', '!./src/scss/**/_*.scss'])
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest('./dest/css/'));
+});
+
+// jadeファイルのコンパイル用タスク
+gulp.task('jade', () => {
+  gulp.src(['./src/jade/**/*.jade', '!./src/jade/**/_*.jade'])
+    .pipe(plumber())
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./dest/'));
+});
+
+// ファイル変更監視タスク
+gulp.task('watch', function(){
+  // ファイルが変更されたらsass/jadeコンパイルタスクを実行
+  gulp.watch('./src/scss/*.scss', ['sass'])
+  gulp.watch('./src/jade/*.jade', ['jade'])
+});
+
+gulp.task('default', ['sass', 'jade', 'watch', 'serve']);
